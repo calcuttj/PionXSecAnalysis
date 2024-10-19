b12b90d24ecc9652e50fb98591688b70b23aa257
Jacob Calcutt <calcuttj@msu.edu>
2024-10-19 12:05:27 -0500

    Adding some update to get_endz_effs

--- get_endz_effs.py
+++ get_endz_effs.py
@@ -6,10 +6,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 RT.gROOT.SetBatch()
 RT.gStyle.SetOptStat(0)
+RT.gStyle.SetErrorX(0.)
 from argparse import ArgumentParser as ap
+
+colors = [
+ 602, 433, 837, 419,
+ 403, 795, 630, 629,
+ 893, 619, 397, 632
+]
 
 def get_vals(t, outname, maxnum=-1):
   ####Efficiency testing
+
+  fOut = RT.TFile(outname.replace('.h5', '.root'), 'recreate')
+  tOut = RT.TTree('tree', 'tree')
+  x_br = np.array([0.])
+  cat_br = np.array([0])
+  y_br = np.array([0.])
+  cos_br = np.array([0.])
+  cos_end_br = np.array([0.])
+  beam_x_br = np.array([0.])
+  beam_y_br = np.array([0.])
+  good_event_br = np.array([0])
+  tOut.Branch('good_event', good_event_br, 'good_event/I')
+  tOut.Branch('cat', cat_br, 'cat/I')
+  tOut.Branch('x', x_br, 'x/D')
+  tOut.Branch('y', y_br, 'y/D')
+  tOut.Branch('cos', cos_br, 'cos/D')
+  tOut.Branch('cos_end', cos_end_br, 'cos_end/D')
+  tOut.Branch('beam_x', beam_x_br, 'beam_x/D')
+  tOut.Branch('beam_y', beam_y_br, 'beam_y/D')
 
   with h5.File(outname, 'w') as h5f:
     dsets = dict()
@@ -60,7 +86,20 @@ def get_vals(t, outname, maxnum=-1):
       for i, zcut in enumerate(zcuts):
 
         in_fv = zl > zcut
-        if not in_fv: continue
+        if not in_fv:
+
+          if i == 0:
+            x_br[0] = -999.
+            y_br[0] = -999.
+            cos_br[0] = -999.
+            cos_end_br[0] = -999.
+            beam_x_br[0] = -999.
+            beam_y_br[0] = -999.
+            cat_br[0] = -999
+            good_event_br[0] = 0
+            tOut.Fill()
+          continue
+        good_event_br[0] = 1
         #print(zcut, np.where(calo_Z >= zcut), np.where(calo_Z >= zcut)[0][0])
         #iz = max(np.where(calo_Z >= zcut)[0][0], 1)
         for iz in range(len(calo_Z)):
@@ -95,19 +134,34 @@ def get_vals(t, outname, maxnum=-1):
         beam_ys[i].append(e.beam_inst_Y + zcut*beam_dirY/beam_dirZ)
         if e.MC:
           true_cat[i].append(e.beam_backtrack)
-
+        if i == 0:
+          x_br[0] = tpc_x 
+          y_br[0] = tpc_y
+          cos_br[0] = -999.
+          cos_end_br[0] = costhetas_end[i][-1]
+          beam_x_br[0] = beam_xs[i][-1] 
+          beam_y_br[0] = beam_ys[i][-1] 
+          if e.MC: cat_br[0] = e.beam_backtrack
+          tOut.Fill()
+         
       if maxnum > 0 and ie > maxnum: break
 
-      #h5f['zs'] = zcuts
-      for i in range(len(zcuts)):
-        dsets['xs'][0] = xs[i]
-        dsets['ys'][0] = ys[i]
-        dsets['beam_xs'][0] = beam_xs[i]
-        dsets['beam_ys'][0] = beam_ys[i]
-        dsets['costhetas'][0] = costhetas[i]
-        dsets['costhetas_end'][0] = costhetas_end[i]
-        dsets['true_cat'][0] = true_cat[i]
+    #h5f['zs'] = zcuts
+    fOut.cd()
+    tOut.Write()
+    fOut.Close()
 
+    for i in range(len(zcuts)):
+      dsets['xs'][0] = xs[i]
+      dsets['ys'][0] = ys[i]
+      dsets['beam_xs'][0] = beam_xs[i]
+      dsets['beam_ys'][0] = beam_ys[i]
+      dsets['costhetas'][0] = costhetas[i]
+      dsets['costhetas_end'][0] = costhetas_end[i]
+      dsets['true_cat'][0] = true_cat[i]
+
+
+     
 def gauss(x, a, x0, sigma):
   return a*np.exp(-(x-x0)**2/(2*sigma**2))
 
@@ -158,14 +212,115 @@ def get_stats(args):
     print(pars[1:])
     #plt.show()
 
+def plot_hist(data, bins, scale=1.):
+  vals, bin_edges = np.histogram(data, bins=data) 
+  bin_centers = 0.5*(bin_edges[1:]+bin_edges[:-1])
+  return vals, bin_centers
+
+
+def draw(t, br_str, name, binning, cut='', scale=1.):
+  draw_str = f'{br_str}>>{name}({",".join([str(s) for s in binning])})'
+  t.Draw(draw_str, cut + ' && {good_event}')
+  h = RT.gDirectory.Get(name)
+  #h.AddBinContent(1, h.GetBinContent(0))
+  #h.SetBinContent(0, 0)
+  #h.AddBinContent(h.GetNbinsX(), h.GetBinContent(h.GetNbinsX()+1))
+  #h.SetBinContent(h.GetNbinsX()+1, 0)
+  h.Scale(scale)
+  return h
+
+def make_stack(hists, name, colors=colors):
+  s = RT.THStack(name, '')
+  for i, h in enumerate(hists):
+    h.SetFillColor(colors[i])
+    h.SetLineColor(colors[i])
+    s.Add(h)
+  return s
+
+def format_data(h):
+  h.SetMarkerStyle(20)
+  #h.SetLineColor(RT.kBlack)
+
+def draw_canvas(smc, hdata, name, xtitle, ytitle):
+  c = RT.TCanvas(name, '')
+  c.SetTicks()
+  smc.Draw()
+  themax = 1.1*max([smc.GetHistogram().GetMaximum(), hdata.GetMaximum()])
+  hdata.SetMaximum(themax)
+  hdata.Draw('pe1')
+  smc.Draw('hist same')
+  hdata.SetTitle('')
+  hdata.GetXaxis().SetTitle(xtitle)
+  hdata.GetYaxis().SetTitle(ytitle)
+  hdata.SetTitleSize(.06, 'XY')
+  hdata.SetTitleOffset(.8, 'XY')
+  RT.gPad.RedrawAxis()
+
+  hdata.Draw('pe1 same')
+  return c
+
+def do_compare(tMC, tData, draw_str, draw_str_data, name_base, xtitle, ytitle, binning, scale, scale_data=1.):
+  hs = []
+  for i in range(1, 7):
+    h = draw(tMC, draw_str, f'hMC_{name_base}_{i}', binning, cut=f'cat=={i}', scale=scale)
+    hs.append(h)
+    h.Write()
+  s = make_stack(hs, f'{name_base}_stack')
+  hd = draw(tData, draw_str_data, f'hData_{name_base}', binning, scale=scale_data)
+  format_data(hd)
+  hd.Write()
+
+  c = draw_canvas(s, hd, f'c{name_base}', xtitle, ytitle, )
+  c.Write()
+
+
 def compare_data_mc(args):
-  fMC = h5.File(args.m)
-  fData = h5.File(args.d)
+  fMC = RT.TFile.Open(args.m)
+  fData = RT.TFile.Open(args.d)
 
-  
+  tMC = fMC.Get('tree')
+  tData = fData.Get('tree')
+  fOut = RT.TFile(args.o, 'recreate')
 
-  fMC.close()
-  fData.close()
+  scale = 1.e-3*tData.GetEntries()/tMC.GetEntries()
+  scale_data = 1.e-3
+
+
+  mcmeanx=-2.18868327
+  mcsigmax= 1.15702154
+  mcmeany= 0.97183832
+  mcsigmay= 1.17307941
+  mcmeanr= 2.71273912
+  mcsigmar= 1.08169567
+  datameanx= 3.0995176
+  datasigmax= 1.36652396
+  datameany= 1.37823363
+  datasigmay= 1.60954298
+  datameanr= 3.72614747
+  datasigmar= 1.38783237
+
+  draw_str = 'x'
+  do_compare(tMC, tData, draw_str, draw_str, 'x',
+             'Reconstructed #DeltaX at FV Face [cm]',
+             'Number of Entries [x10^3]', (30, -60, 60), scale, scale_data)
+  draw_str = 'x - beam_x'
+  do_compare(tMC, tData, draw_str, draw_str, 'deltax',
+             '#DeltaX at FV Face [cm]',
+             'Number of Entries [x10^3]', (20, -20, 20), scale, scale_data)
+  draw_str = f'(x - beam_x - {mcmeanx})/{mcsigmax}'
+  draw_str_data = f'(x - beam_x - {datameanx})/{datasigmax}'
+  do_compare(tMC, tData, draw_str, draw_str_data, 'deltax_norm',
+             'Normalized #DeltaX at FV Face [cm]',
+             'Number of Entries [x10^3]', (10, -5, 5), scale, scale_data)
+
+  #cx.Write()
+
+  #hd.Write()
+  #s.Write()
+
+  fOut.Close()
+  fMC.Close()
+  fData.Close()
 
 if __name__ == '__main__':
   parser = ap()
@@ -175,7 +330,7 @@ if __name__ == '__main__':
   parser.add_argument('-o', default='endz_effs.root', type=str)
   parser.add_argument('--vis', action='store_true')
   parser.add_argument('--routine', type=str, default=None,
-                      choices=['vals', 'stats', None])
+                      choices=['vals', 'stats', 'compare', None])
   parser.add_argument('--n_vals', type=int, default=-1)
   args = parser.parse_args()
 
@@ -188,6 +343,9 @@ if __name__ == '__main__':
     exit()
   elif args.routine == 'stats':
     get_stats(args)
+    exit()
+  elif args.routine == 'compare':
+    compare_data_mc(args)
     exit()
   else:
     pass
